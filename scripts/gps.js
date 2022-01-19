833af4d9fb1e43b6a5ce31ed49f5a7709ca554f0
RodionNikolaev <rodion@screenshotmonitor.com>
2022-01-19 09:21:39 +0300

    Initial commit

---
+++ scripts/gps.js
@@ -0,0 +1,97 @@
+navigator.geolocation.watchPosition((pos) => {
+    try {
+        updadeCurrentPosition(pos.coords);
+    }
+    catch (e) {
+        console.error(e);
+    }
+}, (err) => {
+    console.error(err);
+}, {
+    enableHighAccuracy: true
+});
+
+function dms2dec(lat, latRef, lon, lonRef) {
+    var ref = { 'N': 1, 'E': 1, 'S': -1, 'W': -1 };
+    var sep = [' ,', ' ', ','];
+    var i;
+
+    if (typeof lat === 'string') {
+        for (i = 0; i < sep.length; i++) {
+            if (lat.split(sep[i]).length === 3) {
+                lat = lat.split(sep[i]);
+                break;
+            }
+        }
+    }
+
+    if (typeof lon === 'string') {
+        for (i = 0; i < sep.length; i++) {
+            if (lon.split(sep[i]).length === 3) {
+                lon = lon.split(sep[i]);
+                break;
+            }
+        }
+    }
+
+    for (i = 0; i < lat.length; i++) {
+        if (typeof lat[i] === 'string') {
+            lat[i] = lat[i].split('/');
+            lat[i] = parseInt(lat[i][0], 10) / parseInt(lat[i][1], 10);
+        }
+    }
+
+    for (i = 0; i < lon.length; i++) {
+        if (typeof lon[i] === 'string') {
+            lon[i] = lon[i].split('/');
+            lon[i] = parseInt(lon[i][0], 10) / parseInt(lon[i][1], 10);
+        }
+    }
+
+    lat = (lat[0] + (lat[1] / 60) + (lat[2] / 3600)) * ref[latRef];
+    lon = (lon[0] + (lon[1] / 60) + (lon[2] / 3600)) * ref[lonRef];
+
+    return { lat, lon };
+};
+
+function deg2dms(deg, isLat) {
+    let dplaces = 2;
+    let d = Math.floor(deg);          // make degrees
+    let m = Math.floor((deg - d) * 60);    // make minutes
+    let s = Math.round(((deg - d) * 60 - m) * 60 * Math.pow(10, dplaces)) / Math.pow(10, dplaces); // Make sec rounded
+    s == 60 && (m++, s = 0);            // if seconds rounds to 60 then increment minutes, reset seconds
+    m == 60 && (d++, m = 0);            // if minutes rounds to 60 then increment degress, reset minutes
+
+    let letter = isLat ? (deg > 0 ? "N" : "S") : (deg > 0 ? "E" : "W");
+    return (d + "° " + m + "' " + s + '" ' + letter);   // create output DMS string
+}
+
+function distance(lat1, lon1, lat2, lon2) {
+    const R = 6371e3; // metres
+    const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
+    const φ2 = lat2 * Math.PI / 180;
+    const Δφ = (lat2 - lat1) * Math.PI / 180;
+    const Δλ = (lon2 - lon1) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c; // in metres
+
+    return d;
+}
+
+function bearing(lat1, lon1, lat2, lon2) {
+
+    const φ1 = lat1 * Math.PI / 180; // φ, λ in radians
+    const φ2 = lat2 * Math.PI / 180;
+    const Δλ = (lon2 - lon1) * Math.PI / 180;
+
+    const y = Math.sin(Δλ) * Math.cos(φ2);
+    const x = Math.cos(φ1) * Math.sin(φ2) - Math.sin(φ1) * Math.cos(φ2) * Math.cos(Δλ);
+    const θ = Math.atan2(y, x);
+    const brng = (θ * 180 / Math.PI + 360) % 360; // in degrees
+    return brng;
+}
---
+++ scripts/index.js
@@ -0,0 +1,156 @@
+function rotatePoint(angle, point, cX, cY) {
+    let radians = angle / (180 / Math.PI);
+    return {
+        x: (point.x - cX) * Math.cos(radians) - (point.y - cY) * Math.sin(radians) + cX,
+        y: (point.x - cX) * Math.sin(radians) + (point.y - cY) * Math.cos(radians) + cY
+    }
+}
+
+function shiftPoint(point, length, angle) {
+    return {
+        x: point.x + length * Math.sin(((90 - angle) * Math.PI) / 180.0),
+        y: point.y + length * Math.sin((angle * Math.PI) / 180.0),
+    };
+}
+
+function lineLength(p1, p2) {
+    return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+}
+
+function getDirection(centerPoint, startPoint, endPoint) {
+    return (startPoint.x - centerPoint.x) * (endPoint.y - centerPoint.y) - (startPoint.y - centerPoint.y) * (endPoint.x - centerPoint.x) < 0 ? -1 : 1;
+}
+
+function lineAngle(startPoint, endPoint) {
+    let p1 = { x: startPoint.x + 100000, y: startPoint.y };
+
+    let a = lineLength(p1, startPoint);
+    let b = lineLength(endPoint, startPoint);
+    let c = lineLength(p1, endPoint);
+    let cos = (Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b);
+
+    let direction = getDirection(startPoint, p1, endPoint);
+
+    return direction * (Math.acos(cos > 1 ? 1 : cos) * 180) / Math.PI;
+}
+
+function perpendicularToLine(point, start, end) {
+    const k =
+        ((end.y - start.y) * (point.x - start.x) - (end.x - start.x) * (point.y - start.y)) /
+        (Math.pow(end.y - start.y, 2) + Math.pow(end.x - start.x, 2));
+
+    let p = {
+        x: point.x - k * (end.y - start.y),
+        y: point.y + k * (end.x - start.x)
+    };
+
+    return {
+        p,
+        isInside:
+            p.x >= Math.min(start.x, end.x) &&
+            p.x <= Math.max(start.x, end.x) &&
+            p.y >= Math.min(start.y, end.y) &&
+            p.y <= Math.max(start.y, end.y),
+    };
+};
+
+function shortestrPerp(point, lines) {
+    return lines.map((line, index) => {
+
+        let perp = perpendicularToLine(point, line.p0, line.p1);
+        if (!perp.isInside) return null;
+
+        return {
+            i: index,
+            p: perp.p,
+            l: lineLength(point, perp.p),
+        };
+    }).filter(p => p).sort((p1, p2) => p1.l - p2.l)[0];
+}
+
+function round(number, digits = 3) {
+    return Math.round(number * Math.pow(10, digits)) / Math.pow(10, digits);
+}
+
+var fp = null;
+var settings = null;
+
+let full_gps_distance = null;
+let full_gps_bearing = null;
+
+let full_svg_length = null;
+let full_svg_Angle = null;
+
+let routeLines = null;
+let prev_point = null;
+
+function config(fpSettings) {
+
+    settings = fpSettings;
+
+    let { p0, p1 } = fpSettings;
+
+    full_gps_distance = distance(p0.lat, p0.lon, p1.lat, p1.lon);
+    full_gps_bearing = bearing(p0.lat, p0.lon, p1.lat, p1.lon);
+
+    full_svg_length = lineLength(p0, p1);
+    full_svg_Angle = lineAngle(p0, p1);
+
+    let _fp = new ExpoFP.FloorPlan({
+        element: document.querySelector("#floorplan"),
+        eventId: "park-pobedy",
+        noOverlay: false,
+        onFpConfigured: () => {
+            document.querySelector(".info").classList.add("ready");
+            fp = _fp;
+        },
+        onDirection: (e) => {
+            routeLines = e.lines;
+        }
+    });
+}
+
+function updadeCurrentPosition(coords) {
+
+    let lat = coords.latitude;
+    let lon = coords.longitude;
+
+    if (!lat || !lon || !fp) {
+        prev_point = null;
+        return;
+    }
+
+    let { p0 } = settings;
+
+    let point_distance = distance(p0.lat, p0.lon, lat, lon);
+    let point_bearing = bearing(p0.lat, p0.lon, lat, lon);
+
+    let delta_bearing = point_bearing - full_gps_bearing; // В градусах
+    let delta_distanсe = point_distance / full_gps_distance; // Соотношение от деления
+
+    let location = rotatePoint(delta_bearing, shiftPoint(p0, full_svg_length * delta_distanсe, full_svg_Angle), p0.x, p0.y);
+    fp.selectCurrentPosition(location, !prev_point);
+
+    let left_dist = 0;
+
+    if (routeLines) {
+
+        let perp = shortestrPerp(location, routeLines);
+
+        for (let i = 0; i < routeLines.length; i++) {
+            let line = routeLines[i];
+            let len = lineLength(line.p0, line.p1);
+
+            if (!perp || perp.i > i) left_dist += len;
+        }
+
+        if (perp) left_dist += lineLength(routeLines[perp.i].p0, perp.p);
+    }
+
+    let speed = coords.speed > 0.5 ? round(coords.speed * 3.6, 1) : "--";
+    let left_perc = round(left_dist * full_gps_distance / full_svg_length / 1000, 2) || "--";
+
+    document.querySelector(".first").innerHTML = `${deg2dms(lat, true)}<br/>${deg2dms(lon)}`;
+    document.querySelector(".second").innerHTML = `${speed} km/h<br/>${left_perc} km`;
+    prev_point = location;
+}
